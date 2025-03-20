e92efbf8625cadcbe6a5d0d08161fe7f70303aac
Mathilde4 <mathibassadou@gmail.com>
2025-03-20 17:49:03 +0000

    modification des paramètres de base de donnée

--- super_wash_car/super_wash_car/settings.py
+++ super_wash_car/super_wash_car/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'super_wash_car.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'car_wash_db',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER':'root',
         'PASSWORD':'',
         'HOST':'localhost',
