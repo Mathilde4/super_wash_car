a26a566b7eb7271ad5f0a29ca78f90167c9707c8
Mathilde4 <mathibassadou@gmail.com>
2025-03-20 16:46:26 +0000

    creation des models

--- super_wash_car/app_car/models.py
+++ super_wash_car/app_car/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    nom = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=8,unique=True)
+    points_fidelite=models.PositiveIntegerField(default=0)
+
+
+class Service(models.Model):
+    nom =models.CharField(max_length=100)
+    description = models.TextField(max_length=50)
+    prix =models.DecimalField(max_digits=10,decimal_places=2)
+
+
+class RendezVous(models.Model):
+    Client = models.ForeignKey(Client,on_delete=models.CASCADE)
+    service = models.ForeignKey(Service,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    rappel_envoye = models.BooleanField(default=False)
